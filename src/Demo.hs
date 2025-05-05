15081f59be3fd7d00a8e3c13e32b61e2c62b7434
BryanGitman <brygitman@gmail.com>
2025-05-05 08:51:55 -0300

    modele espina, pamela y los poderes de espina

--- src/Demo.hs
+++ src/Demo.hs
@@ -10,4 +10,21 @@ data Personaje = UnPersonaje {
     superPoder :: Poder,
     superPoderActivo :: Bool,
     cantidadDeVida :: Int
-}
+} deriving (Show)
+
+espina :: Personaje
+espina = UnPersonaje "Espina" bolaEspinosa (granadaDeEspinas 5) True 4800
+
+pamela :: Personaje
+pamela = UnPersonaje "Pamela" (lluviaDeTuercas "sanadoras") torretaCurativa False 9600
+
+bolaEspinosa :: Poder
+bolaEspinosa personaje@(UnPersonaje { cantidadDeVida = vidaActual }) 
+  | (vidaActual >= 1000) = personaje { cantidadDeVida = vidaActual - 1000 }
+  | otherwise = personaje
+
+granadaDeEspinas :: Int -> Personaje -> Personaje
+granadaDeEspinas radioDeExplosion personaje@(UnPersonaje { nombre = nombreActual, cantidadDeVida = vidaActual })
+  | (radioDeExplosion > 3 && vidaActual < 800) = personaje { nombre = nombreActual + " Espina estuvo aqui", cantidadDeVida = 0, superPoderActivo = False }
+  | (radioDeExplosion > 3) = personaje { nombre = nombreActual + " Espina estuvo aqui" }
+  | otherwise = bolaEspinosa personaje
